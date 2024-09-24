a6840ce613ef3b847cde90065a4dee09e0d4a75b
Paula <114782303+alu0101540863@users.noreply.github.com>
2024-09-24 18:08:55 +0100

    Terminado tools

---
+++ practica1/src/c++/tools/tools.cc
@@ -0,0 +1,52 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * 3º Curso en Grado en Ingeniería Informática
+ * Inteligencia Artificial
+ * Práctica 1: Búsquedas no informadas
+ *
+ * @author Paula Díaz Jorge alu0101540863@ull.edu.es
+ * @date 24 sep 2024
+*/
+
+#include "tools.h"
+
+/**
+ * @brief Función que revisa que el número de parámetros instroducido es correcto
+ * @param argc Número de parámetros
+ * @param argv Parámetros introducidos
+ * @param nombre_fichero Nombre del fichero de entrada
+ * @return El código de error
+*/
+int CheckParams(int argc, char* argv[], std::string& nombre_fichero) {
+  if (argc != 2) {
+    return -1;
+  } else {
+    nombre_fichero = argv[1];
+    return 0;
+  }
+}
+
+/**
+ * @brief Función que controla los errores y sus mensajes
+ * @param error El código de error de la comprobación de parámetros
+ * @return El código de error
+*/
+int CheckErrors(const int& error) {
+  if (error == -1) {
+    std::cerr << "Error: Se ha introducido un número de parámetros no esperado" << std::endl;
+    Help();
+  }
+}
+
+/**
+ * @brief Función que imprime información sobre el programa
+*/
+void Help() {
+  std::cout << "Modo de empleo: ./bin/BusquedasNoInformadas [fichero]" << std::endl;
+  std::cout << "Programa que implementa búsquedas no informadas para encontrar un camino entre dos vértices de un grafo" << std::endl;
+  std::cout << "Formato del fichero:" << std::endl;
+  std::cout << "  Primera fila: número de vértices" << std::endl;
+  std::cout << "  Siguientes filas: se enumeran las distancias entre los pares de vértices" << std::endl;
+  std::cout << "Si no hay una arista que conecte el vértice i con el vértice j, la distancia es -1" << std::endl;
+}
---
+++ practica1/src/c++/tools/tools.h
@@ -0,0 +1,17 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * 3º Curso en Grado en Ingeniería Informática
+ * Inteligencia Artificial
+ * Práctica 1: Búsquedas no informadas
+ *
+ * @author Paula Díaz Jorge alu0101540863@ull.edu.es
+ * @date 24 sep 2024
+*/
+
+#include <string>
+#include <iostream>
+
+int CheckParams(int argc, char* argv[], std::string&);
+int CheckErrors(const int&);
+void Help();
